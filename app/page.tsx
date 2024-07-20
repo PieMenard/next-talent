cfa7a88ca031c09093299f50b0496db648e59b78
PieMenard <pinedo.m.gustavo@gmail.com>
2024-07-20 00:20:40 -0400

    add talent card

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import AddTalent from '@/components/AddTalent';
+import ShowTalent from '@/components/ShowTalent';
 import { prisma } from '@/utils/prisma';
 
 export default async function Home() {
@@ -9,8 +10,14 @@ export default async function Home() {
       <h1 className="text-6xl font-bold text-center">💼 Next Talent</h1>
       <p>Hire the best around!</p>
       <AddTalent />
-      {talentData.length > 0 &&
-        talentData.map((talent) => <div key={talent.id}>{talent.name}</div>)}
+      <div className="flex flex-wrap justify-between w-10/12 mx-auto gap-4 mt-8">
+        {talentData.length > 0 &&
+          talentData.map((talent) => (
+            <div key={talent.id}>
+              <ShowTalent talentData={talent} />
+            </div>
+          ))}
+      </div>
     </main>
   );
 }
---
+++ components/ShowTalent.tsx
@@ -0,0 +1,40 @@
+import { Talent } from '@prisma/client';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+const ShowTalent = ({ talentData }: { talentData: Talent }) => {
+  return (
+    <div>
+      <Card className="w-[300px] h-[330px]">
+        <CardHeader>
+          <CardTitle className="text-4xl">{talentData.name}</CardTitle>
+          <CardDescription>{talentData.title}</CardDescription>
+          <CardDescription>{talentData.email}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>{talentData.skills}</p>
+          {/* Delete Button */}
+          <div className="flex gap-4 my-4">
+            <form action="">
+              <input type="hidden" name="inputID" value={talentData.id} />
+              <button className="bg-red-500 px-6 py-2 rounded text-white">
+                Delete
+              </button>
+            </form>
+          </div>
+          {/* Dialog */}
+          {/* Edit */}
+          {/* Hire Button */}
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default ShowTalent;
