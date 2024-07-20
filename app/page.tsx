55c37ce33a26c307193ef3285ea4259da41b1f4b
PieMenard <pinedo.m.gustavo@gmail.com>
2024-07-19 22:18:10 -0400

    change font

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-6xl font-bold text-center">💼 Next Talent</h1>
+      <p>Hire the best around!</p>
+    </main>
   );
 }
